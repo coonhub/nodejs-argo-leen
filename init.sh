c2d285fd9ad5d1ec26b362bb7c56ace4ec7e1be8
leenchen <leenchen@nmg.com.hk>
2026-01-28 12:34:39 +0800

    update init.sh

--- init.sh
+++ init.sh
@@ -2,22 +2,20 @@
 CUR_DIR=$(cd "$(dirname $0)"; pwd)
 
 mkdir -p /app
-cd $CUR_DIR
-cp Cli index.js ws.js xhttp.js package.json nezha.sh /app/
-
-cd /app
-npm install || exit 1
+cp $CUR_DIR/Cli $CUR_DIR/index.js $CUR_DIR/ws.js $CUR_DIR/xhttp.js $CUR_DIR/package.json $CUR_DIR/nezha.sh /app/
 apk update || exit 1
 apk upgrade || exit 1
 apk add --no-cache bash openssl curl gcompat iproute2 coreutils libstdc++ libgcc icu-libs supervisor uuidgen || exit 1
+chmod +x $CUR_DIR/nezha.sh
+$CUR_DIR/nezha.sh || exit 1
 
-chmod +x /app/nezha.sh
-/app/nezha.sh || exit 1
 mkdir -p /etc/supervisor.d
-cp cli.ini /etc/supervisor.d/cli.ini
-cp nezha.ini /etc/supervisor.d/nezha.ini
-cp node*.ini /etc/supervisor.d/
-cp start.sh /start.sh
+cp $CUR_DIR/cli.ini /etc/supervisor.d/cli.ini
+cp $CUR_DIR/nezha.ini /etc/supervisor.d/nezha.ini
+cp $CUR_DIR/node*.ini /etc/supervisor.d/
+cp $CUR_DIR/start.sh /start.sh
 chmod +x /start.sh || exit 1
 
+cd /app
+npm install || exit 1
 exit 0
