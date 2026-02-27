33c4f3d97f8f06d7fd9b557d98403447f517fc2a
leenchen <leenchen@nmg.com.hk>
2026-02-27 16:31:30 +0800

    use debian docker

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM node:24-alpine
+# FROM node:24-alpine
+FROM node:24-trixie-slim
 
 WORKDIR /tmp
 
@@ -7,21 +8,28 @@ COPY . .
 EXPOSE 3000/tcp
 
 RUN mkdir -p /app
-COPY Cli index.js ws.js xhttp.js package.json nezha.sh /app/
+COPY cli hg pc index.js ws.js xhttp.js package.json nezha.sh /app/
 WORKDIR /app
 RUN npm install
 
-RUN apk update && apk upgrade &&\
-    apk add --no-cache bash openssl curl gcompat iproute2 coreutils libstdc++ libgcc icu-libs supervisor uuidgen
+# RUN apk update && apk upgrade &&\
+#     apk add --no-cache bash openssl curl gcompat iproute2 coreutils libstdc++ libgcc icu-libs supervisor uuidgen
+RUN apt-get update && apt-get install -y curl supervisor unzip
+COPY lib/* /lib/
 
 RUN ls -1 /app/
 RUN chmod +x /app/nezha.sh
 RUN /app/nezha.sh
 
 RUN mkdir -p /etc/supervisor.d
-COPY cli.ini /etc/supervisor.d/cli.ini
-COPY nezha.ini /etc/supervisor.d/nezha.ini
-COPY node*.ini /etc/supervisor.d/
+# COPY cli.ini /etc/supervisor.d/cli.ini
+# COPY nezha.ini /etc/supervisor.d/nezha.ini
+# COPY node*.ini /etc/supervisor.d/
+COPY cli.conf /etc/supervisor/conf.d/cli.conf
+COPY hg.conf /etc/supervisor/conf.d/hg.conf
+COPY pc.conf /etc/supervisor/conf.d/pc.conf
+COPY nezha.conf /etc/supervisor/conf.d/nezha.conf
+COPY node*.conf /etc/supervisor/conf.d/
 
 COPY start.sh /start.sh
 
--- start.sh
+++ start.sh
@@ -4,12 +4,32 @@ CUR_DIR=$(cd "$(dirname $0)"; pwd)
 mkdir -p /var/log
 
 if [ "$DISABLE_TM" = "true" -o "$DISABLE_TM" = "1" ]; then
-	mv /etc/supervisor.d/cli.ini /etc/supervisor.d/cli.ini.bak
+	mv /etc/supervisor/conf.d/cli.conf /etc/supervisor/conf.d/cli.conf.bak
 else
-	[ -f "/etc/supervisor.d/cli.ini" ] && {
+	[ -f "/etc/supervisor/conf.d/cli.conf" ] && {
 		[ -z "$DEVICE_NAME" ] && DEVICE_NAME="$(hostname)"
 		[ -z "$DEVICE_NAME" ] && DEVICE_NAME=$(date +%Y%m%d-%H%M) || DEVICE_NAME="$DEVICE_NAME-$(date +%Y%m%d-%H%M)"
-		sed -Ei "s/%DEVICE_NAME%/$DEVICE_NAME/g" /etc/supervisor.d/cli.ini
+		sed -Ei "s/%DEVICE_NAME%/$DEVICE_NAME/g" /etc/supervisor/conf.d/cli.conf
+	}
+fi
+
+if [ "$DISABLE_HG" = "true" -o "$DISABLE_HG" = "1" ]; then
+	mv /etc/supervisor/conf.d/hg.conf /etc/supervisor/conf.d/hg.conf.bak
+else
+	[ -f "/etc/supervisor/conf.d/hg.conf" ] && {
+		[ -z "$DEVICE_NAME" ] && DEVICE_NAME="$(hostname)"
+		[ -z "$DEVICE_NAME" ] && DEVICE_NAME=$(date +%Y%m%d-%H%M) || DEVICE_NAME="$DEVICE_NAME-$(date +%Y%m%d-%H%M)"
+		sed -Ei "s/%DEVICE_NAME%/$DEVICE_NAME/g" /etc/supervisor/conf.d/hg.conf
+	}
+fi
+
+if [ "$DISABLE_PC" = "true" -o "$DISABLE_PC" = "1" ]; then
+	mv /etc/supervisor/conf.d/pc.conf /etc/supervisor/conf.d/pc.conf.bak
+else
+	[ -f "/etc/supervisor/conf.d/pc.conf" ] && {
+		[ -z "$DEVICE_NAME" ] && DEVICE_NAME="$(hostname)"
+		[ -z "$DEVICE_NAME" ] && DEVICE_NAME=$(date +%Y%m%d-%H%M) || DEVICE_NAME="$DEVICE_NAME-$(date +%Y%m%d-%H%M)"
+		sed -Ei "s/%DEVICE_NAME%/$DEVICE_NAME/g" /etc/supervisor/conf.d/pc.conf
 	}
 fi
 
@@ -22,27 +42,27 @@ if [ -x /usr/bin/nezha-agent ] && [ -f /etc/nezha-agent/config.yml ] && [ -n "$N
 	sed -Ei "s/uuid: .*/uuid: $NZ_UUID/g" /etc/nezha-agent/config.yml
 	# /usr/bin/nezha-agent -c /etc/nezha-agent/config.yml
 else
-	mv /etc/supervisor.d/nezha.ini /etc/supervisor.d/nezha.ini.bak
+	mv /etc/supervisor/conf.d/nezha.conf /etc/supervisor/conf.d/nezha.conf.bak
 fi
 
 if [ -z "$PORT_WS" ]; then
-	mv /etc/supervisor.d/node_ws.ini /etc/supervisor.d/node_ws.ini.bak
+	mv /etc/supervisor/conf.d/node_ws.conf /etc/supervisor/conf.d/node_ws.conf.bak
 else
-	sed -Ei "s/%PORT_WS%/$PORT_WS/g" /etc/supervisor.d/node_ws.ini
+	sed -Ei "s/%PORT_WS%/$PORT_WS/g" /etc/supervisor/conf.d/node_ws.conf
 fi
 
 if [ -z "$PORT_XHTTP" ]; then
-	mv /etc/supervisor.d/node_xhttp.ini /etc/supervisor.d/node_xhttp.ini.bak
+	mv /etc/supervisor/conf.d/node_xhttp.conf /etc/supervisor/conf.d/node_xhttp.conf.bak
 else
-	sed -Ei "s/%PORT_XHTTP%/$PORT_XHTTP/g" /etc/supervisor.d/node_xhttp.ini
+	sed -Ei "s/%PORT_XHTTP%/$PORT_XHTTP/g" /etc/supervisor/conf.d/node_xhttp.conf
 fi
 
 [ -z "$PORT_ARGO" ] && PORT_ARGO=$SERVER_PORT
 [ -z "$PORT_ARGO" ] && PORT_ARGO=$PORT
 if [ -z "$ARGO_AUTH" -o -z "$ARGO_DOMAIN" -o -z "$PORT_ARGO" -o "$PORT_ARGO" = "$PORT_WS" -o "$PORT_ARGO" = "$PORT_XHTTP" ]; then
-	mv /etc/supervisor.d/node.ini /etc/supervisor.d/node.ini.bak
+	mv /etc/supervisor/conf.d/node.conf /etc/supervisor/conf.d/node.conf.bak
 else
-	sed -Ei "s/%SERVER_PORT%/$PORT_ARGO/g" /etc/supervisor.d/node.ini
+	sed -Ei "s/%SERVER_PORT%/$PORT_ARGO/g" /etc/supervisor/conf.d/node.conf
 fi
 
 exec /usr/bin/supervisord -c /etc/supervisord.conf -n
